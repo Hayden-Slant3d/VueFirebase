8827fc8a40e755bb5d044b99abc58bfa7beced46
Hayden-Slant3d <136860245+Hayden-Slant3d@users.noreply.github.com>
2023-07-01 00:11:36 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAV7mO58IrdBUfJfV6Lall2mww1u6vO2P0",
-    authDomain: "barcode-dd952.firebaseapp.com",
-    projectId: "barcode-dd952",
-    storageBucket: "barcode-dd952.appspot.com",
-    messagingSenderId: "981273260032",
-    appId: "1:981273260032:web:33dfe1875dc8253670e8e5",
-    measurementId: "G-BJ7JWM2MEF"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
